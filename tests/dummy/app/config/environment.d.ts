420724e7d0f72f68bc7927e9d93bb427e0001026
Jan Buschtöns <jan@buschtoens.me>
2021-09-29 11:26:28 +0200

    fix(ts): remove nifty `environment.d.ts` trick
    https://github.com/typed-ember/ember-cli-typescript/blob/master/blueprint-files/ember-cli-typescript/__config_root__/config/environment.d.ts

--- tests/dummy/app/config/environment.d.ts
+++ tests/dummy/app/config/environment.d.ts
@@ -1,8 +1,14 @@
-declare const config: ReturnType<typeof import('../../config/environment')> & {
-  // Injected at build time by addons.
-  APP: {
-    name: string;
-    version: string;
-  };
-};
 export default config;
+
+/**
+ * Type declarations for
+ *    import config from 'my-app/config/environment'
+ */
+declare const config: {
+  environment: string;
+  modulePrefix: string;
+  podModulePrefix: string;
+  locationType: string;
+  rootURL: string;
+  APP: Record<string, unknown>;
+};
